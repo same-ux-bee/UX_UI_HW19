a299da7b0d48aeed08c33b784ff25cea47daadf3
same-ux-bee <68659746+same-ux-bee@users.noreply.github.com>
2024-02-22 17:17:20 -0600

    JS & jQuery Interactions Added

--- js/index.js
+++ js/index.js
@@ -1,26 +1,51 @@
 console.log("External JS file LINKED!!!!");
 
+//smooth scrolling to My Work and My Skills Sections
 $(document).ready(function(){
-    // Add smooth scrolling to all links
     $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
-        // Prevent default anchor click behavior
         event.preventDefault();
-  
-        // Store hash
         var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
+        }, 300, function(){
           window.location.hash = hash;
         });
-      } // End if
+      }
     });
   });
+
+//animates button to become larger when clicked
+  $(".primarybtn").click(function(){
+    $(this).animate({fontSize: '1em'});
+  });
+
+//nav links hover styling
+  $("a").mouseenter(function(){
+    $(this).css({
+      "color": "gold",
+      "font-weight":"500"
+    });
+  });
+  $("a").mouseleave(function(){
+    $(this).css({
+      "color": "white",
+      "font-weight":"300"
+    });
+  });
+
+
+  // BACK TO TOP BUTTON
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
